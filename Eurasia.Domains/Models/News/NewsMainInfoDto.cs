94c0e2ef7c0317a4344e3593ba93480d8c39dc84
JennZen <zlodard@gmail.com>
2026-03-31 16:41:58 +0300

    Add entity NewsData + actions for it

---
+++ Eurasia.Domains/Models/News/NewsMainInfoDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eurasia.Domains.Models.News
+{
+    public class NewsMainInfoDto
+    {
+        public int Id { get; set; }
+
+        //ENUM CATEGORY public string Category { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime PublishedAt { get; set; }
+    }
+}
