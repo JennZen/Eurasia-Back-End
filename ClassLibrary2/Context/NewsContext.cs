0ad06831a7731c96fe92484846e782ebf1bc1e54
JennZen <zlodard@gmail.com>
2026-04-04 22:22:15 +0300

    Connect db to the project and changed News for db

---
+++ ClassLibrary2/Context/NewsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Eurasia.Domains.Entities.News;
+using Eurasia.DataAccess;
+
+namespace Eurasia.DataAccess.Context
+{
+    public class NewsContext: DbContext
+    {
+        public DbSet<NewsData> News { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbSession.ConnectionString);
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,22 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
-//configure database connection
-Eurasia.DataAccess.DbSession.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-//add services to the container
+
+// Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//configure the HTTP request pipeline
-//if (app.Environment.IsDevelopment())
-//{
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
