af486962cef9ba41a3e3258f77ad2e0009c587d2
Bereznitskiy <jenea989@gmail.com>
2026-04-06 01:14:34 +0300

    Forgot to add inheritance to the MainDto.

--- Eurasia.Domains/Models/Country/CountryMainInfoDto.cs
+++ Eurasia.Domains/Models/Country/CountryMainInfoDto.cs
@@ -2,18 +2,8 @@
 
 namespace Eurasia.Domains.Models.Country
 {
-    public class CountryMainInfoDto
+    public class CountryMainInfoDto: CountryBaseDto
     {
         public int Id { get; set; }
-        public required string Name { get; set; }
-        public string? FormalName { get; set; }
-        public string? FlagUrl { get; set; }
-        public int Population { get; set; }
-        public List<Continents>? Continents { get; set; }
-        public string? Currency { get; set; }
-        public List<string>? Regions { get; set; }
-        public required string Capital { get; set; }
-        public int GeographicalSize { get; set; }
-        public List<string>? Languages { get; set; }
     }
 }
